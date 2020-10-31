ad6f4c722f67c764f44ccfe712fa11451226e368
Danil Shashkov <shashkovdanil@gmail.com>
2020-10-31 13:04:38 +0500

    add images

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,18 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
+    <div>
+      <Image src="https://api.released.at/uploads/release/cover/18138/MV5BNWQ5YjI3ZmMtYTRhZS00NjEzLWFkMjYtYmE0Nzc5YmNhY2I1XkEyXkFqcGdeQXVyMjQxNTMwMTU-._V1_SY1000_CR006661000_AL_.jpg" alt="Text" unsized />
+      <Image src="https://api.released.at/uploads/release/cover/18229/voin-2-sezon.jpg" alt="Text" unsized />
+      <Image src="https://api.released.at/uploads/release/cover/18232/primal.jpg" alt="Text" unsized />
+      <Image src="https://api.released.at/uploads/release/cover/18233/the_good_lord.jpg" alt="Text" unsized />
+      <Image src="https://api.released.at/uploads/release/cover/18237/walking-dead-world-beyond.jpg" alt="Text" unsized />
+      <Image src="https://api.released.at/uploads/release/cover/18238/The-Haunting-of-Hill-House-768x432-1.jpg" alt="Text" unsized />
+      <Image src="https://api.released.at/uploads/release/cover/18239/6-things-wed-like-to-see-from-fear-the-walking-dead-in-season-6.jpg" alt="Text" unsized />
+      <Image src="https://api.released.at/uploads/release/cover/18240/Star-Trek-Discovery.jpg" alt="Text" unsized />
     </div>
   )
 }
